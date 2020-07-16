c95917f23940c205272ae55fd38609eb08897ba0
jiilan <60308631+jiilan@users.noreply.github.com>
2020-07-16 18:58:09 +0700

    Add files via upload

---
+++ Kamis malem.py
@@ -0,0 +1,8 @@
+import sys,time
+def jalan (kata):
+    for e in kata:
+       sys.stdout.write(e)
+       sys.stdout.flush()
+       time.sleep(0.1)
+jalan("Halo Pak saya juga programer").center(44))
+jalan("jadi tolong stop sadap saya karena saya tau").center(44))
